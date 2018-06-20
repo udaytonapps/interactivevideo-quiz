8f02d46077fc568e83f92e17a6bb27a5c1981f76
James W <washingtonj3@udayton.edu>
2018-06-20 15:28:05 -0400

    TSUGI-25 Add additional buttons styling Changes to the styling of the new buttons and dropdown

--- play-video.php
+++ play-video.php
@@ -86,20 +86,22 @@ if ($finished) {
                     <button id="backTen" class="btn btn-warning" disabled="disabled" onclick="IntVideo.backTenSeconds()">
                         <span class="fa fa-undo" aria-hidden="true" title="Back Ten Seconds"></span> 10<span class="sr-only">Back 10 Seconds</span>
                     </button>
-                    <div class="dropdown">
-                        <button class="btn btn-info">Select Speed</button>
-                        <div class="dropdown-content">
-                            <a href="#" onclick="IntVideo.changeSpeed(0.25)" value="0.25">0.25</a>
-                            <a href="#" onclick="IntVideo.changeSpeed(0.5)" value="0.5">0.5</a>
-                            <a href="#" onclick="IntVideo.changeSpeed(1)" value="1">1</a>
-                            <a href="#" onclick="IntVideo.changeSpeed(1.5)" value="1.5">1.5</a>
-                            <a href="#" onclick="IntVideo.changeSpeed(2)" value="2">2</a>
-                        </div>
+                    <div class="dropdown speedDropdown">
+                        <button  data-toggle="dropdown" class="dropdownButton">
+                            <span>Select Speed <span class="caret speedDropdownCaret"></span></button>
+                        <ul class="dropdown-menu speedDropdown-menu dropdown-menu-left ">
+                            <li id="speed025"><a href="#" onclick="IntVideo.changeSpeed(0.25)" value="0.25">0.25</a></li>
+                            <li id="speed05"><a href="#" onclick="IntVideo.changeSpeed(0.5)" value="0.5">0.5</a></li>
+                            <li id="speed1" class="speedDropdown-selected"><a href="#" onclick="IntVideo.changeSpeed(1)" value="1">1</a></li>
+                            <li id="speed15"><a href="#" onclick="IntVideo.changeSpeed(1.5)" value="1.5">1.5</a></li>
+                            <li id="speed2"><a href="#" onclick="IntVideo.changeSpeed(2)" value="2">2</a></li>
+                        </ul>
                     </div>
-                    <button id="captionButton" class="btn btn-info"  onclick="IntVideo.toggleCaptions()">
+
+                    <button id="captionButton" class="btn btn-icon"  onclick="IntVideo.toggleCaptions()">
                         <span class="fa fa-cc " aria-hidden="true" title="Captions"></span><span class="sr-only">Turn Captions On/Off</span>
                     </button>
-                    <button id="fullScreenButton" class="btn btn-info"  captions = "true" onclick="IntVideo.toggleFullScreen()">
+                    <button id="fullScreenButton" class="btn btn-icon"  captions = "true" onclick="IntVideo.toggleFullScreen()">
                         <span id="fullScreenSpan" class="fa fa-expand" aria-hidden="true" title="FullScreen"></span><span class="sr-only">Full Screen</span>
                     </button>
                     <span class="pull-right" id="currentPlayTime"></span>
