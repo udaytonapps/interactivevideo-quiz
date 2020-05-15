6f4289fe39cc1a21f6378fa7c059f6b4b4ab9ccb
David Bauer <dbauer1@udayton.edu>
2020-05-15 16:34:25 -0400

    Update register file

--- register.php
+++ register.php
@@ -5,6 +5,23 @@ $REGISTER_LTI2 = array(
     "FontAwesome" => "fa-play-circle", // Icon for the tool
     "short_name" => "Interactive Video",
     "description" => "This tool allows a user to add questions to a Warpwire video.", // Tool description
-    "messages" => array("launch", "launch_grade"),
-    "hide_from_store" => false
+    "messages" => array("launch"),
+    "hide_from_store" => false,
+    "privacy_level" => "public",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English",
+    ),
+    "source_url" => "https://github.com/udaytonapps/interactivevideo-quiz",
+    // For now Tsugi tools delegate this to /lti/store
+    "placements" => array(
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+    )
 );
