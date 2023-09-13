652a0c5e7341e2eb109ab57108c56836c7655585
David Bauer <dbauer1@udayton.edu>
2023-09-13 15:23:57 -0400

    Another PHP 8.2 related issue

--- model/Question.php
+++ model/Question.php
@@ -6,6 +6,7 @@ class Question {
     public $questionId;
     public $videoId;
     public $questionTime;
+    public $questionType;
     public $questionText;
     public $correctFeedback;
     public $incorrectFeedback;
